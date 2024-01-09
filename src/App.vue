f4edc9b04cbfe8d18ec2162824d10f6215d7ef45
Vara-U <vara.uswaransigul@g2-travel.com>
2024-01-09 19:12:04 +0000

    delete

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank"> </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>Test</div>
 </template>
 
 <style scoped>
